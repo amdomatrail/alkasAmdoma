99b64cce700165d933fc7667aac02e27b2c3ec86
guillaume <amdomatrail@gmail.com>
2023-02-28 16:28:10 +0100

    commit

---
+++ formationAlkas/guillaume/php/symfony/tests/Controller/VoitureControllerTest.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Test\Controller;
+
+use App\Entity\Voiture;
+use App\Repository\VoitureRepository;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class VoitureControllerTest extends WebTestCase
+{
+    private KernelBrowser $client;
+    private VoitureRepository $repository;
+    private string $path = '/voiture/';
+
+    protected function setUp(): void
+    {
+        $this->client = static::createClient();
+        $this->repository = static::getContainer()->get('doctrine')->getRepository(Voiture::class);
+
+        foreach ($this->repository->findAll() as $object) {
+            $this->repository->remove($object, true);
+        }
+    }
+
+    public function testIndex(): void
+    {
+        $crawler = $this->client->request('GET', $this->path);
+
+        self::assertResponseStatusCodeSame(200);
+        self::assertPageTitleContains('Voiture index');
+
+        // Use the $crawler to perform additional assertions e.g.
+        // self::assertSame('Some text on the page', $crawler->filter('.p')->first());
+    }
+
+    public function testNew(): void
+    {
+        $originalNumObjectsInRepository = count($this->repository->findAll());
+
+        $this->markTestIncomplete();
+        $this->client->request('GET', sprintf('%snew', $this->path));
+
+        self::assertResponseStatusCodeSame(200);
+
+        $this->client->submitForm('Save', [
+            'voiture[nom]' => 'Testing',
+        ]);
+
+        self::assertResponseRedirects('/voiture/');
+
+        self::assertSame($originalNumObjectsInRepository + 1, count($this->repository->findAll()));
+    }
+
+    public function testShow(): void
+    {
+        $this->markTestIncomplete();
+        $fixture = new Voiture();
+        $fixture->setNom('My Title');
+
+        $this->repository->save($fixture, true);
+
+        $this->client->request('GET', sprintf('%s%s', $this->path, $fixture->getId()));
+
+        self::assertResponseStatusCodeSame(200);
+        self::assertPageTitleContains('Voiture');
+
+        // Use assertions to check that the properties are properly displayed.
+    }
+
+    public function testEdit(): void
+    {
+        $this->markTestIncomplete();
+        $fixture = new Voiture();
+        $fixture->setNom('My Title');
+
+        $this->repository->save($fixture, true);
+
+        $this->client->request('GET', sprintf('%s%s/edit', $this->path, $fixture->getId()));
+
+        $this->client->submitForm('Update', [
+            'voiture[nom]' => 'Something New',
+        ]);
+
+        self::assertResponseRedirects('/voiture/');
+
+        $fixture = $this->repository->findAll();
+
+        self::assertSame('Something New', $fixture[0]->getNom());
+    }
+
+    public function testRemove(): void
+    {
+        $this->markTestIncomplete();
+
+        $originalNumObjectsInRepository = count($this->repository->findAll());
+
+        $fixture = new Voiture();
+        $fixture->setNom('My Title');
+
+        $this->repository->save($fixture, true);
+
+        self::assertSame($originalNumObjectsInRepository + 1, count($this->repository->findAll()));
+
+        $this->client->request('GET', sprintf('%s%s', $this->path, $fixture->getId()));
+        $this->client->submitForm('Delete');
+
+        self::assertSame($originalNumObjectsInRepository, count($this->repository->findAll()));
+        self::assertResponseRedirects('/voiture/');
+    }
+}
